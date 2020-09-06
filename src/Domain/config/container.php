c9231c7546ed3797095f75016345f944e6862538
Vitaliy <theyamshikov@yandex.ru>
2020-09-06 19:29:55 +0600

    upd

---
+++ src/Domain/config/container.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'PhpBundle\\Jwt\\Domain\\Interfaces\\Repositories\\ProfileRepositoryInterface' => 'PhpBundle\\Jwt\\Domain\\Repositories\\Config\\ProfileRepository',
+    'PhpBundle\\Jwt\\Domain\\Interfaces\\Services\\JwtServiceInterface' => 'PhpBundle\\Jwt\\Domain\\Services\\JwtService',
+];
