4107327c785e7d265931de0596608c6c3667ef89
Vitaliy <theyamshikov@yandex.ru>
2020-09-27 16:55:43 +0600

    upd

--- src/Domain/Repositories/Config/ProfileRepository.php
+++ src/Domain/Repositories/Config/ProfileRepository.php
@@ -7,14 +7,14 @@ use ZnCore\Domain\Helpers\EntityHelper;
 use ZnCore\Base\Enums\Measure\TimeEnum;
 use ZnCrypt\Jwt\Domain\Entities\JwtProfileEntity;
 use ZnCrypt\Jwt\Domain\Entities\KeyEntity;
-use ZnCore\Base\Libs\Env\DotEnvHelper;
+use ZnCore\Base\Libs\DotEnv\DotEnv;
 
 class ProfileRepository implements ProfileRepositoryInterface
 {
 
     public function oneByName(string $profileName)
     {
-        $prifile = DotEnvHelper::get('jwt.profiles.' . $profileName);
+        $prifile = DotEnv::get('jwt.profiles.' . $profileName);
         $keyEntity = new KeyEntity;
         EntityHelper::setAttributes($keyEntity, $prifile['key']);
         $profileEntity = new JwtProfileEntity;
