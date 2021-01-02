25f8343a6a90f165de4ad9adfc4b660a268dcc67
Vitaliy <theyamshikov@yandex.ru>
2021-01-02 17:53:36 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'ZnCrypt\\Jwt\\Domain\\Interfaces\\Repositories\\ProfileRepositoryInterface' => 'ZnCrypt\\Jwt\\Domain\\Repositories\\Config\\ProfileRepository',
-    'ZnCrypt\\Jwt\\Domain\\Interfaces\\Services\\JwtServiceInterface' => 'ZnCrypt\\Jwt\\Domain\\Services\\JwtService',
+    'ZnCrypt\Jwt\Domain\Interfaces\Services\JwtServiceInterface' => 'ZnCrypt\Jwt\Domain\Services\JwtService',
+    'ZnCrypt\Jwt\Domain\Interfaces\Repositories\ProfileRepositoryInterface' => 'ZnCrypt\Jwt\Domain\Repositories\Config\ProfileRepository',
 ];
